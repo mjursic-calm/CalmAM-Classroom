29b78781f9069d428ed26268cc1ad4ba1481cdb0
mjursic-calm <mjursic@gmail.com>
2026-08-17 23:06:28 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'calmabc-v1';
+const urlsToCache = ['/CalmAM-Classroom/'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
